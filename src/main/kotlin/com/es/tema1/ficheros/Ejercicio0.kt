fb80e1ef2c952b338054870006171e6138706825
Lainezz <diegolinaresmojacar@gmail.com>
2024-09-26 17:27:38 +0200

    adicion resto

---
+++ src/main/kotlin/com/es/tema1/ficheros/Ejercicio0.kt
@@ -0,0 +1,35 @@
+package com.es.tema1.ficheros
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import kotlin.io.path.notExists
+
+
+fun main() {
+
+    /*
+    Mover el archivo prueba.txt a la carpeta CarpetaPrueba
+     */
+
+    val rutaRaiz = Path.of("src")
+    val rutaFichero = rutaRaiz.resolve("main").resolve("resources").resolve("prueba.txt")
+
+    val rutaDestino = rutaRaiz.resolve("main")
+        .resolve("resources")
+        .resolve("CarpetaPrueba")
+        .resolve("documentoDestino.txt")
+
+    if (rutaDestino.notExists()) {
+        // Creo los directorios hasta la ruta destino
+        Files.createDirectories(rutaDestino.parent)
+        // Creo el fichero documentoDestino.txt
+        Files.createFile(rutaDestino)
+    }
+    // Una vez creado el fichero destino y la ruta hacia el mismo, puedo copiar el contenido
+    Files.copy(rutaFichero, rutaDestino, StandardCopyOption.REPLACE_EXISTING)
+
+}
+
+
+
---
+++ src/main/kotlin/com/es/tema1/ficheros/LecturaEscritura.kt
@@ -0,0 +1,48 @@
+package com.es.tema1.ficheros
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+fun main() {
+
+
+    val ficheroPrueba = Path.of("src/main/resources/prueba.txt")
+    // Vamos a explorar cómo leer un archivo con BufferedReader
+    // Debemos crear/abrir un flujo de lectura de tipo BufferedReader
+    val br: BufferedReader = Files.newBufferedReader(ficheroPrueba)
+
+//    // Una vez tenemos el flujo de lectura abierto, podemos consumirlo
+//    br.forEachLine { line -> println(line) }
+//    // IMPORTANTE cerrar los flujos
+//    br.close()
+
+    // Una manera más segura de recorrer todas las líneas es usando el .use
+    // El .use asegura que aunque haya excepciones, se cierra el flujo
+    br.use { flujo ->
+        flujo.forEachLine {
+            // line -> println(it)
+            println(it)
+        }
+    }
+
+    // BUFFERED WRITER
+    val ficheroPrueba2 = Path.of("src/main/resources/prueba2.txt")
+    // Vamos a explorar cómo ESCRIBIR un archivo con BufferedWriter
+    // Debemos crear/abrir un flujo de escritura de tipo BufferedWriter
+    val bw: BufferedWriter = Files.newBufferedWriter(ficheroPrueba2)
+
+    // .use para asegurar que liberamos el recurso
+    bw.use { flujo ->
+        flujo.write("Hola Mundo Writer")
+        flujo.append("Hola Mundo Writer1")
+        flujo.append("Hola Mundo Writer2")
+        flujo.append("Hola Mundo Writer3")
+        flujo.append("Hola Mundo Writer4")
+        flujo.newLine()
+        flujo.append("Hola Mundo Writer4")
+
+    }
+}
